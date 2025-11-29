248cca2248eb7ac6aff773d1554030edeb5fb7e1
abugroon <moawia22010@hotmail.com>
2025-11-29 18:18:26 +0300

    +updates

--- public/build/assets/CardDescription.vue_vue_type_script_setup_true_lang-DZVoSe1w.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./index-VjrIMgiF.js";import{d as t,a as r,A as n,n as c,e as p,o as l}from"./app-BC767CvY.js";const f=t({__name:"CardDescription",props:{class:{}},setup(s){const e=s;return(a,m)=>(l(),r("p",{"data-slot":"card-description",class:c(p(o)("text-muted-foreground text-sm",e.class))},[n(a.$slots,"default")],2))}});export{f as _};
